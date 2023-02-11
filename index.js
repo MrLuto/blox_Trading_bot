dee9017bb0ff26ebb73c7a0fbf87fe4f2806e00e
MrLuto <mmudde2@gmail.com>
2023-02-11 20:47:51 +0100

    oops

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ var run = async function run(){
     await page.click('button.btn.css-1kk59s6');
     await delay(500);
     login = await page.$('input#username');
-    await login.type("lutobeheer@gmail.com");
+    await login.type("usernameeee");
     login = await page.$('input#password');
-    await login.type("Mudde112");
+    await login.type("passworddd");
     await page.click('input#kc-login');
     await delay(500);
     while(true){
